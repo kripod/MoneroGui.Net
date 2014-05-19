fa2d527b3c4013d2b67ce2c6d3c4a9a902d3f887
Jojatekok <jojatekokweb@gmail.com>
2014-05-19 17:56:08 +0200

    Initial commit

---
+++ Monero Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information 
+// associated with an assembly.
+[assembly: AssemblyTitle("Monero Client")]
+[assembly: AssemblyDescription("A GUI for interacting with the command line version of Monero.")]
+[assembly: AssemblyCompany("Jojatekok")]
+[assembly: AssemblyProduct("Monero Client")]
+[assembly: AssemblyCopyright("Copyright © Jojatekok 2014")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components. If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// In order to begin building localizable applications, set 
+// <UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file 
+// inside a <PropertyGroup>. For example, if you are using US english 
+// in your source files, set the <UICulture> to en-US. Then uncomment 
+// the NeutralResourceLanguage attribute below. Update the "en-US" in 
+// the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, // Where theme specific resource dictionaries are located
+    // (Used if a resource is not found in the page, or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly // Where the generic resource dictionary is located
+    //(Used if a resource is not found in the page, app, or any theme specific resource dictionaries)
+)]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: GuidAttribute("1ed6dec2-f23e-4880-aaa4-4b2b6ce74def")]
---
+++ README.md
@@ -0,0 +1,24 @@
+# Monero Client
+This project provides a Graphical User Interface (GUI) to interact with the command line version of Monero.
+The program is written in [C#][] and [XAML][] ([WPF][]).
+
+[C#]: http://wikipedia.org/wiki/C_Sharp_%28programming_language%29
+[XAML]: http://wikipedia.org/wiki/XAML
+[WPF]: http://wikipedia.org/wiki/Windows_Presentation_Foundation
+
+## Usage requirements
+Installing [Microsoft .NET Framework 4.5][] (or higher) is a requirement for both end-users and developers.
+
+[Microsoft .NET Framework 4.5]: http://www.microsoft.com/download/details.aspx?id=30653
+
+## About versioning
+The project uses [Semantic Versioning 2.0.0][] for keeping track of changes. It is not in relation with the command line application's version number.
+
+[Semantic Versioning 2.0.0]: http://semver.org/spec/v2.0.0.html
+
+## Donations
+Donations are highly appreciated in order to keep the development alive.
+
+- __Bitcoin:__ 14wHehBtFt321WTV15khon8Juaxh9drnfJ
+- __BlackCoin:__ B9nBaQSFV4Ns5L8Jze8vaxc176zeE8XjDQ
+- __Monero:__ 47hK4gehaMrFTQCiV5FEmM54hpqTrdHudb9nUBG88NicBDpxH4TGuh3TmW84Dc6dpMiEiBLGvJCuT3xC3LNHctmx7mG8NLM
